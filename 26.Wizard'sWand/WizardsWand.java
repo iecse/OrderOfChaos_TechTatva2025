bb10d02a62450f8d4a5a75b75886fcff81da1114
Kushal <rajkushaloff@gmail.com>
2025-08-29 19:55:34 +0530

    java code update

--- 26.Wizard'sWand/WizardsWand.java
+++ 26.Wizard'sWand/WizardsWand.java
@@ -1,4 +1,3 @@
-import java.util.*;
 
 class WizardsdWand{
 
@@ -43,18 +42,5 @@ class WizardsdWand{
         }
 
         return ans;
-    }
-
-    @SuppressWarnings("resource")
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        long[] arr = new long[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextLong();
-        }
-
-        System.out.println(wizardWand(arr));
     }
 }
