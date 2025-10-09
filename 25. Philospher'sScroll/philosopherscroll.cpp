26d5cc19098e929438336ef29bf8a13e7d60cb14
r1shu-R <iamworstpro@gmail.com>
2025-10-10 03:32:26 +0530

    philosopher's scroll solution changed

--- 25. Philospher'sScroll/philosopherscroll.cpp
+++ 25. Philospher'sScroll/philosopherscroll.cpp
@@ -1,51 +1,49 @@
-#include <iostream>  // For cout, cin, ios::sync_with_stdio, cin.tie
-#include <string>    // For std::string
-#include <stack>     // For std::stack
-#include <vector>    // Often useful, but implicitly included by stack/string headers on some systems. Best to keep standard headers explicit.
-
+#include <iostream>
+#include <stack>
+#include <string>
 using namespace std;
 
-string decodeScroll(const string& s) {
-    stack<int> numStack;
-    stack<string> strStack;
-    string currStr;
-    int currNum = 0;
+string decodeString(const string &s) {
+    stack<int> counts;
+    stack<string> resultStack;
+    string current = "";
+    int k = 0;
 
-    for (char c : s) {
-        if (isdigit(c)) {
-            currNum = currNum * 10 + (c - '0');  // build number
-        } else if (c == '[') {
-            numStack.push(currNum);
-            strStack.push(currStr);
-            currNum = 0;
-            currStr.clear();
-        } else if (c == ']') {
-            int repeat = numStack.top(); numStack.pop();
-            string prevStr = strStack.top(); strStack.pop();
-            string expanded;
-            for (int j = 0; j < repeat; j++) {
-                expanded += currStr;
-            }
-            currStr = prevStr + expanded;
+    for (char ch : s) {
+        if (isdigit(ch)) {
+            k = k * 10 + (ch - '0'); // build the repeat number
+        } else if (ch == '[') {
+            counts.push(k);
+            resultStack.push(current);
+            current = "";
+            k = 0;
+        } else if (ch == ']') {
+            int repeat = counts.top(); counts.pop();
+            string prev = resultStack.top(); resultStack.pop();
+            string temp = "";
+            for (int i = 0; i < repeat; i++) temp += current;
+            current = prev + temp;
         } else {
-            currStr.push_back(c); // append character
+            current += ch; // normal character
         }
     }
-    return currStr;
+
+    return current;
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    string s;
-    cout << "Enter the magical scroll: ";
-    // Note: cin >> s; will only read up to the first whitespace. 
-    // If the scroll can contain spaces, you should use getline(cin, s);
-    cin >> s; 
+    int t;
+    cin >> t;
+    cin.ignore(); // ignore newline after t
 
-    string decoded = decodeScroll(s);
-    cout << "Decoded spell: " << decoded << "\n";
+    while (t--) {
+        string s;
+        getline(cin, s);
+        cout << decodeString(s) << "\n";
+    }
 
     return 0;
 }
