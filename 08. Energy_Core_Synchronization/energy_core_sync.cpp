9afa521859bc3e07cc3a590235b64c70470627e3
r1shu-R <iamworstpro@gmail.com>
2025-10-10 03:17:07 +0530

    changed solution

--- 08. Energy_Core_Synchronization/energy_core_sync.cpp
+++ 08. Energy_Core_Synchronization/energy_core_sync.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 using namespace std;
 
+int minRituals(long long X) {
+    int bits = 0;
+    long long temp = X;
+    while (temp > 0) {
+        bits++;
+        temp >>= 1; // divide by 2
+    }
+    return 2 * bits + 1;
+}
+
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int t;
     cin >> t;
     while (t--) {
-        long long x;
-        cin >> x;
-
-        int bits = 0;
-        long long temp = x;
-        while (temp > 0) {
-            bits++;
-            temp >>= 1; // divide by 2
-        }
-
-        // answer is number of bits + 2
-        cout << bits + 2 << "\n";
+        long long X;
+        cin >> X;
+        cout << minRituals(X) << "\n";
     }
     return 0;
 }
